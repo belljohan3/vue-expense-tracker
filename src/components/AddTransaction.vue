529209b0a7f7455c1f767a77334d31140f51f21c
Johan Bell <belljohan3@gmail.com>
2024-02-13 21:30:38 +0100

    Add transaction, delete transaction and save to the localstorage

---
+++ src/components/AddTransaction.vue
@@ -0,0 +1,51 @@
+<template>
+  <h3>Add new transaction</h3>
+  <form id="form">
+    <div class="form-control">
+      <label for="text">Text</label>
+      <input type="text" id="text" v-model="text" placeholder="Enter text..." />
+    </div>
+    <div class="form-control">
+      <label for="amount"
+        >Amount <br />
+        (negative - expense, positive - income)</label
+      >
+      <input
+        type="text"
+        id="amount"
+        v-model="amount"
+        placeholder="Enter amount..."
+      />
+    </div>
+    <button class="btn" @click.prevent="onSubmit">Add transaction</button>
+  </form>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import { useToast } from "vue-toastification";
+
+const toast = useToast();
+
+const text = ref("");
+const amount = ref("");
+
+const emit = defineEmits(["transactionSubmitted"]);
+
+const onSubmit = () => {
+  if (!text.value || !amount.value) {
+    toast.error("Both fields must be filled");
+    return;
+  }
+
+  const transactionData = {
+    text: text.value,
+    amount: parseFloat(amount.value),
+  };
+
+  emit("transactionSubmitted", transactionData);
+
+  text.value = "";
+  amount.value = "";
+};
+</script>
